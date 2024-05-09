0625b068b7c139e4dbfd7c9e8f88f05f2966b059
Maria Vasquez <mfvasquez17@outlook.com>
2024-05-09 07:54:47 -0500

    se agrega carrusel

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+import { LocalService } from 'src/app/services/crypto/local.service';
+import * as Constants from '../../shared/Constant';
 import { ProductComponent } from '../product/product.component';
-
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -14,10 +17,37 @@ export class HomeComponent {
   isSidebarActive = false;
   isHeaderActive = false;
 
-  constructor() { }
+  constructor(private userService: UserService,
+    private storageService: LocalService) { }
 
   ngOnInit(): void {
+    this.getUser();
 
+  }
+
+  getUser() {
+    const token = this.storageService.getJsonValue(Constants.USER_TOKEN);
+    if (token != null) {
+      if (this.storageService.getJsonValue(Constants.USER_DATA) == null) {
+        const observable: Observable<any> = this.userService.getUser(token, this.storageService.getJsonValue(Constants.USER_LOGIN).username);
+        observable.subscribe({
+          next: async (resp: any) => {
+            this.storageService.setJsonValue(Constants.USER_DATA, resp.data);
+            window.location.reload();
+          }
+        });
+      }
+    }
+
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
+
+  toggleSidebar() {
+    this.isSidebarActive = !this.isSidebarActive;
+    this.isHeaderActive = !this.isHeaderActive;
   }
 
 
--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbCarouselModule],
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
