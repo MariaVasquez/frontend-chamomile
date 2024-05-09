e3d090ceaf2869fcb4e3a74c24f3090c9792ac1a
Maria Vasquez <mfvasquez17@outlook.com>
2024-05-08 21:22:44 -0500

    se organiza confid

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from '../product/product.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [ProductComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  data: any;
+  isSidebarActive = false;
+  isHeaderActive = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
 
 }
